a83acef7fb387a70338c80300cd124f5bf75798d
Mrinal Singh <singhmrinal19@gmail.com>
2020-09-03 21:23:04 +0530

    till lec 25 #2

--- src/main/java/com/apps/ui/controller/UserController.java
+++ src/main/java/com/apps/ui/controller/UserController.java
@@ -1,10 +1,13 @@
-package com.apps.mobileappws;
+package com.apps.ui.controller;
 
+
+import com.apps.ui.model.request.UserDetailsRequestModel;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,7 @@ public class UserController {
         return "get user was called";
     }
     @PostMapping
-    public String createUser(){
+    public String createUser(@RequestBody UserDetailsRequestModel userDetails){
         return "create user was called";
     }
 
